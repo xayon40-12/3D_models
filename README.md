b85809c09e2c90b11d1a1bd29eb9238494003831
xayon40-12 <touroux.nathan@gmail.com>
2025-10-19 16:08:39 +0200

    add junctionO

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # 3D-models
 
 Some 3D models made with code in [OpenSCAD](https://openscad.org) and [waterfall CAD](https://github.com/joe-warren/opencascade-hs?tab=readme-ov-file#-1).
+
+## Waterfall-CAD models
+![MinimalJunction.hs](waterfall_cad/MinimalJunction.svg)
--- waterfall_cad/MinimalJunction.hs
+++ waterfall_cad/MinimalJunction.hs
@@ -13,62 +13,53 @@
 import qualified Waterfall as W
 import qualified Waterfall.SVG.ToSVG as W
 import Linear
-import Control.Lens
 
 
--- module knob(w, tolerance = 0) {
--- 	let (t = tolerance) {
--- 		translate([0, 0, w/4+t]) cylinder(w/4, w/2, w/8);
--- 		translate([0, 0, -w/4-E-t]) cylinder(w/2+2*(t+E), w/2, w/2);
--- 		translate([0, 0, -w/2-t]) cylinder(w/4, w/8, w/2);
--- 	}
--- }
-
--- module junction_o(l, w, tolerance) {
--- 	difference() {
--- 		union() {
--- 			cube([l, w, w], center = true);
--- 			translate([(-l+w)/2+0.15*w, 0, 0]) cube([1.3*w, 1.3*w, w], center = true);
--- 		}
--- 		translate([(-l+w)/2, 0, 0]) tol() cube([w, w/2+w/16, w], center=true);
--- 		translate([(-l+w)/2, 0, 0]) rotate([-90, 0, 0]) tol() knob(w, tolerance);
--- 		difference() {
--- 			translate([-l/2+w/4, 0, 0]) tol() cube([w/2, 2*w, w], center=true);
--- 			translate([(-l+w)/2, 0, 0]) rotate([-90, 0, 0]) cylinder(h = 2*w, r = w/2, center = true);
--- 		}
--- 	}
--- }
-
--- module junction_i(l, w) {
--- 	difference() {
--- 		cube([l, w, w], center = true);
--- 		translate([(-l+w)/2, 0, 0]) tol() cube([w, w, w], center=true);
--- 	}
--- 	translate([-l/2+w*3/4, 0, 0]) tol() cube([w/2, w/2, w], center=true);
--- 	translate([(-l+w)/2, 0, 0]) rotate([-90, 0, 0]) tol() knob(w);
--- }
-
-knob :: Double -> W.Solid
-knob w = W.uScale w . W.rotate (V3 1 0 0) (pi/2) . W.translate (V3 0 0 (-0.375)) $
-    (W.translate (V3 0 0 0) . W.uScale 0.5) W.unitCone
+knobI :: Double -> W.Solid
+knobI w = W.uScale w . W.rotate (V3 1 0 0) (pi/2) . W.translate (V3 0 0 (-0.5)) $
+    (W.translate (V3 0 0 0.125) . W.uScale 0.25) W.unitCone
     `W.union`
-    (W.translate (V3 0 0 0.25) . W.scale (V3 0.5 0.5 0.25)) W.unitCylinder
+    (W.translate (V3 0 0 0.875) . W.rotate (V3 1 0 0) pi . W.uScale 0.25) W.unitCone
     `W.union`
-    (W.translate (V3 0 0 0.75) . W.rotate (V3 1 0 0) pi . W.uScale 0.5) W.unitCone
+    (W.translate (V3 0 0 0.375) . W.scale (V3 0.5 0.5 0.25)) W.unitCylinder
     `W.union`
-    (W.translate (V3 (-0) (-0.5) 0.25) . W.scale (V3 0.5 1 0.25)) W.unitCube
+    (W.translate (V3 (-0) (-0.5) 0.375) . W.scale (V3 0.5 1 0.25)) W.unitCube
+
+knobO :: Double -> W.Solid
+knobO w = W.uScale w . W.rotate (V3 1 0 0) (pi/2) . W.translate (V3 0 0 (-0.5)) $
+    (W.translate (V3 0 0 0) . W.scale (V3 0.5 0.5 1)) W.unitCylinder
+    `W.union`
+    (W.translate (V3 (-0) (-0.5) 0) . W.scale (V3 0.5 1 1)) W.unitCube
+    `W.difference`
+    (W.translate (V3 (-0.5) (-0.5) (0.375)) . W.scale (V3 1 1 0.25)) W.unitCube
+    `W.difference`
+    (W.translate (V3 0 0 0.125) . W.uScale 0.25) W.unitCone
+    `W.difference`
+    (W.translate (V3 0 0 0.875) . W.rotate (V3 1 0 0) pi . W.uScale 0.25) W.unitCone
+    `W.difference`
+    let h = 0.5 in
+    (W.translate (V3 (-0.5) 0 0.5) . W.rotate (V3 1 0 0) (pi/4) . W.scale (V3 0.5 (h/sqrt 2) (h/sqrt 2)) . W.translate (V3 0 (-0.5) (-0.5))) W.unitCube
 
 junctionI :: Double -> Double -> W.Solid
 junctionI l w =
-    (W.translate (V3 (l/2-w) 0 0) . W.scale (V3 (l-w) w w)) W.centeredCube
+    (W.translate (V3 (w/2) 0 0) . W.scale (V3 (l-w) w w)) W.centeredCube
     `W.union`
-    (W.translate (V3 (-l/2+w) 0 0)) (knob w)
+    (W.translate (V3 ((w-l)/2) 0 0)) (knobI w)
+
+junctionO :: Double -> Double -> W.Solid
+junctionO l w = let d = l/2-w in
+    (W.translate (V3 (-w/2) 0 0) . W.scale (V3 (l-w) w w)) W.centeredCube
+    `W.union`
+    (W.translate (V3 ((l-w)/2) 0 0) . W.rotate (V3 0 0 1) pi) (knobO w)
 
 minimalJunction :: Double -> Double -> W.Solid
-minimalJunction l w = junctionI l w
+minimalJunction l w = let d = 0.5*l in
+    W.translate (V3 d 0 0) (junctionI l w)
+    `W.union`
+    W.translate (V3 (-d-w) 0 0) (junctionO l w)
 
 main :: IO ()
 main = do
     W.writeSTL 0.001 "MinimalJunction.stl" $ minimalJunction 20 5
-    W.writeDiagramSVG "MinimalJunction.svg" $ W.solidDiagram (V3 0 0 (-1)) $ minimalJunction 200 50
+    W.writeDiagramSVG "MinimalJunction.svg" $ W.solidDiagram (V3 1 (-1) 1) $ minimalJunction 200 50
 
